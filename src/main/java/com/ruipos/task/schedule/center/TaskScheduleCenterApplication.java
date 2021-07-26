22c4dde5469cb1d8f5b8542328df9b174304ce6e
臣不贰 <notochendevelop@gmail.com>
2021-07-26 13:42:52 +0800

    Initial commit

---
+++ src/main/java/com/ruipos/task/schedule/center/TaskScheduleCenterApplication.java
@@ -0,0 +1,15 @@
+package com.ruipos.task.schedule.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduleCenterApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TaskScheduleCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruipos/task/schedule/center/TaskScheduleCenterApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ruipos.task.schedule.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskScheduleCenterApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
